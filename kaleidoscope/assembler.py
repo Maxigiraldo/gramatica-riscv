e0e9752c2cfadf3aef7f0889540691374eff519e
max <maximilianogo2806@gmail.com>
2024-05-06 13:33:03 -0500

    First commit

---
+++ kaleidoscope/assembler.py
@@ -0,0 +1,193 @@
+import plyplus
+import sys
+
+regNames = {
+  'zero': 0,
+  'ra': 1,
+  'x0': 0, 
+  'x1': 1,
+  'x2': 2,
+  'x2': 2,
+  'x3': 3,
+  'x4': 4,
+  'x5': 5,
+  'x6': 6,
+  'x7': 7,
+  'x8': 8,
+  'x9': 9,
+  'x10': 10, 
+  'x11': 11,
+  'x12': 12,
+  'x12': 12,
+  'x13': 13,
+  'x14': 14,
+  'x15': 15,
+  'x16': 16,
+  'x17': 17,
+  'x18': 18,
+  'x19': 19,
+  'x20': 20, 
+  'x21': 21,
+  'x22': 22,
+  'x22': 22,
+  'x23': 23,
+  'x24': 24,
+  'x25': 25,
+  'x26': 26,
+  'x27': 27,
+  'x28': 28,
+  'x29': 29,
+  'x30': 30,
+  'x31': 31
+}
+
+instInfo = {
+  'add': {
+    'opcode': '0110011',
+    'f3': '000',
+    'f7': '0000000'
+  },
+  'sub': {
+    'opcode': '0110011',
+    'f3': '000',
+    'f7': '0100000'
+  },
+  'xor': {
+    'opcode': '0110011',
+    'f3': '100',
+    'f7': '0000000'
+  },
+  'or': {
+    'opcode': '0110011',
+    'f3': '110',
+    'f7': '0000000'
+  },
+  'and': {
+    'opcode': '0110011',
+    'f3': '111',
+    'f7': '0000000'
+  },
+  'sll': {
+    'opcode': '0110011',
+    'f3': '001',
+    'f7': '0000000'
+  },
+  'srl': {
+    'opcode': '0110011',
+    'f3': '101',
+    'f7': '0000000'
+  },
+  'sra': {
+    'opcode': '0110011',
+    'f3': '101',
+    'f7': '0100000'
+  },
+  'slt': {
+    'opcode': '0110011',
+    'f3': '010',
+    'f7': '0000000'
+  },
+  'sltu': {
+    'opcode': '0110011',
+    'f3': '011',
+    'f7': '0000000'
+  },
+  'mul': {
+    'opcode': '0110011',
+    'f3': '000',
+    'f7': '0000001'
+  },
+  'mulh': {
+    'opcode': '0110011',
+    'f3': '001',
+    'f7': '0000001'
+  },
+  'mulsu': {
+    'opcode': '0110011',
+    'f3': '010',
+    'f7': '0000001'
+  },
+  'mulu': {
+    'opcode': '0110011',
+    'f3': '011',
+    'f7': '0000001'
+  },  
+  'div': {
+    'opcode': '0110011',
+    'f3': '100',
+    'f7': '0000001'
+  },
+  'divu': {
+    'opcode': '0110011',
+    'f3': '101',
+    'f7': '0000001'
+  },
+  'rem': {
+    'opcode': '0110011',
+    'f3': '110',
+    'f7': '0000001'
+  },
+  'remu': {
+    'opcode': '0110011',
+    'f3': '111',
+    'f7': '0000001'
+  }
+  
+}
+
+class HVisitor(plyplus.STransformer):
+  def __init__(self):
+    super().__init__()
+    self.instructionCounter = 0
+    self.labels = {}
+  def label(self, expr):
+    self.labels[expr.tail[0]] = self.instructionCounter + 4
+  def program(self, expr):
+    print("Nodo programa")    
+    print(self.instructionCounter)
+    print(expr)
+    
+  def regname(self, expr):
+    val = expr.tail[0]
+    regNum = regNames[val]
+    regBin = format(int(regNum),'05b')
+    return regBin
+  
+  def instr(self, expr):
+    # Instruction name
+    inst =  expr.tail[0]
+  
+    return {
+      'opcode': instInfo[inst]['opcode'],
+      'f3': instInfo[inst]['f3'],
+      'f7': instInfo[inst]['f7']
+    }
+    
+  def imm(self, expr):
+    val = expr.tail[0]
+    print("sextend(" + val +")", end='')
+
+  def inst(self, expr):
+    
+    print("-{}--".format(self.instructionCounter))
+    self.instructionCounter = self.instructionCounter + 4
+    
+    print("opcode({})-rd({})-f3({})-rs1({})-rs2({})-f7({})".format(expr.tail[0]['opcode'], expr.tail[1],expr.tail[0]['f3'] ,expr.tail[2],expr.tail[3],expr.tail[0]['f7']))
+    print("-xxx-")
+    
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print("Example call: {} input.asm output.asm".format(sys.argv[0]))
+  else:
+    sourceFile = sys.argv[1]
+    targetFile = sys.argv[2]
+    with open('riscv.g', 'r') as grm:
+      with open(sourceFile, 'r') as scode:
+        parser = plyplus.Grammar(grm)
+        source = scode.read();
+        t = parser.parse(source)
+        #t.to_png_with_pydot(r"tree.png")
+        v = HVisitor()
+        v.transform(t)
+        print(v.labels)
---
+++ kaleidoscope/riscv.g
@@ -0,0 +1,71 @@
+start: program;
+
+program:
+  inst;
+ 
+
+inst: 
+    instr  regname ',' regname ',' regname // R instruction
+  | insti  regname ',' regname ',' imm     // I instruction
+  | instil regname ',' offset // IL instruction
+  | insts regname ',' offset ',' regname // S instruction
+  | instb regname ',' regname ',' imm // B instruction
+  | instj imm // J instruction
+  | instu regname ',' imm // U instruction
+  ;
+
+offset: imm '\(' regname '\)';
+
+instname: instr | insti;
+
+instr: 'add' | 'sub' | 'xor' | 'or' | 'and' | 'sll' | 'srl' | 'sra' | 'slt' | 'sltu' | 'mul' | 'mulh' | 'mulsu' | 'mulu' | 'div' | 'divu' | 'rem' | 'remu';
+insti: 'addi' | 'xori' | 'ori' | 'andi' | 'slli' | 'srli' | 'srai' | 'slti' | 'sltiu' | 'jalr' | 'ecall' | 'ebreak';
+instil: 'lw' | 'lh' | 'lb' | 'lbu' | 'lhu';
+insts: 'sw' | 'sh' | 'sb';
+instb: 'beq' | 'bne' | 'blt' | 'bge' | 'bltu' | 'bgeu';
+instj: 'jal';
+instu: 'lui' | 'auipc';
+
+imm : VAL;
+
+VAL: '[0]|(\-|\+)?[1-9][0-9]*';
+
+
+regname: 
+   'x0' | 
+  | 'x1' | 'ra'
+  | 'x2' | 'sp'
+  | 'x3' | 'gp'
+  | 'x4' | 'tp'
+  | 'x5' | 't0' 
+  | 'x6' | 't1'
+  | 'x7' | 't2'
+  | 'x8' | 's0' | 'fp'
+  | 'x9' | 's1'
+  | 'x10' | 'a0' 
+  | 'x11' | 'a1'
+  | 'x12' | 'a2' 
+  | 'x13' | 'a3' 
+  | 'x14' | 'a4' 
+  | 'x15' | 'a5' 
+  | 'x16' | 'a6' 
+  | 'x17' | 'a7'
+  | 'x18' | 's2' 
+  | 'x19' | 's3' 
+  | 'x20' | 's4' 
+  | 'x21' | 's5' 
+  | 'x22' | 's6' 
+  | 'x23' | 's7' 
+  | 'x24' | 's8' 
+  | 'x25' | 's9' 
+  | 'x26' | 's10' 
+  | 'x27' | 's11'
+  | 'x28' | 't3' 
+  | 'x29' | 't4' 
+  | 'x30' | 't5' 
+  | 'x31' | 't6'
+;
+
+
+COMMENT: '[\;\#]' '.*?' (%ignore);
+WS: '[ \t\r\n]+' (%ignore);
